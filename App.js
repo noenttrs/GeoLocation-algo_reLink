825671a1d584f0e4bac415e8d2fce445cb1e0875
noenttrs <nottarisnoe@gmail.com>
2022-12-13 09:12:39 +0100

    Updates

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Platform, Text, View, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 import * as Location from 'expo-location';
 
@@ -7,7 +8,7 @@ export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
-  useEffect(() => {
+
     (async () => {
       
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -18,19 +19,40 @@ export default function App() {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
-    })();
-  }, []);
+    })()
+
+
 
   let text = 'Waiting..';
+  let latitude = 0;
+  let longitude = 0;
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
-    text = JSON.stringify(location);
+    console.log(location)
+    latitude = location.coords.latitude;
+    longitude = location.coords.longitude;
+    console.log(latitude, longitude)
+    text = JSON.stringify(location, null, 4);
   }
 
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>{text}</Text>
+      <MapView 
+      style={styles.map}
+      initialRegion={{
+        latitude: 48.866667,
+        longitude: 2.333333,
+        latitudeDelta: 0.922,
+        longitudeDelta: 0.421,
+      }}>
+        
+        <Marker
+          key = "ma position"
+          coordinate={{ latitude : latitude , longitude : longitude }}
+        />
+        </MapView>
     </View>
   );
 }
@@ -42,4 +64,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '40%',
+  },
+
 });
